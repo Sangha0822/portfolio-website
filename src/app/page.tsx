d9464ad21b70a04288a08359c293a26b83cb956b
Sangha Jeon <sangha082214@gmail.com>
2025-05-09 22:35:37 -0700

    added door icon and created main portfolio page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,29 @@
 'use client'
 
 import { useState } from 'react'
+import { useRouter } from 'next/navigation'
+import { motion, AnimatePresence } from 'framer-motion'
 
 export default function Home() {
   const [showPage, setShowPage] = useState(false)
-  
-  if(!showPage) {
+  const router = useRouter()
     return (
       <main className="flex flex-col items-center justify-center min-h-screen">
+
         <button
-          onClick={() => setShowPage(true)}
-          className="bg-yellow-600 text-2xl text-textColor-light font-bold rounded-lg hover:opacity-80 w-48 h-12"
-        >
-          Enter Site
+          onClick={() => router.push('/portfolio')}
+          className="flex items-center justify-center">
+          <img 
+            src="/door.png" 
+            alt="Door icon" 
+            className="text-2xl text-textColor-light font-bold rounded-lg hover:opacity-80 w-32 h-36" 
+          />
         </button>
-        <h1 className="text-4xl font-bold text-textColor-light">
+
+        <h1 className="text-4xl font-extrabold text-textColor-light">
           Not much to see here... unless you want more.
         </h1>
-        <p className="text-lg text-gray-700">(To learn more about me ↑)</p>
+        <p className="text-lg font-sans text-gray-700">(To learn more about me ↑)</p>
       </main>
     );
-  } else {
-    return (
-      <main className="flex flex-col items-center justify-center min-h-screen">
-        <h1>Hello</h1>
-      </main>
-    )
-  }
 }
---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,9 @@
+// src/app/portfolio/page.tsx
+export default function Portfolio() {
+    return (
+      <main className="min-h-screen">
+        <h1 className="text-4xl font-bold">Welcome to my Portfolio!</h1>
+        {/* Add your portfolio content here */}
+      </main>
+    );
+  }
